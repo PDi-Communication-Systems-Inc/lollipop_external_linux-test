81cb59182c391d2ea8db45c94f0f40111794d5fb
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2017-02-02 09:09:40 -0500

    Pull in Android.mk parts that are missing from master branch

--- test/mxc_v4l2_test/Android.mk
+++ test/mxc_v4l2_test/Android.mk
@@ -3,6 +3,33 @@ ifeq ($(HAVE_FSL_IMX_IPU),true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := mxc_v4l2_capture.c
+LOCAL_CFLAGS += -DBUILD_FOR_ANDROID
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_SHARED_LIBRARIES := libutils libc
+LOCAL_MODULE := mxc-v4l2-capture
+LOCAL_MODULE_TAGS := tests
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := mxc_v4l2_overlay.c
+LOCAL_CFLAGS += -DBUILD_FOR_ANDROID
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_SHARED_LIBRARIES := libutils libc
+LOCAL_MODULE := mxc-v4l2-overlay
+LOCAL_MODULE_TAGS := tests
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := mxc_v4l2_output.c
+LOCAL_CFLAGS += -DBUILD_FOR_ANDROID
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_SHARED_LIBRARIES := libutils libc
+LOCAL_MODULE := mxc-v4l2-output
+LOCAL_MODULE_TAGS := tests
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
 #LOCAL_SRC_FILES := mxc_v4l2_tvin.c
 # mxc_v4l2_tvin_pdi.c is a attached file in PDI community on Nov 3, 2016 2:47 AM (https://community.nxp.com/thread/428784?messageTarget=all&start=50&mode=comments)
 #LOCAL_SRC_FILES := mxc_v4l2_tvin_pdi.c
